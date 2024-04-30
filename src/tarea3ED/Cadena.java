c99d6f8a005920a29f72254678e6be809252283c
itsazahara <aromsch1403@g.educaand.es>
2024-05-01 00:02:11 +0200

    lanzamiento del repositorio

---
+++ src/tarea3ED/Cadena.java
@@ -0,0 +1,15 @@
+package tarea3ED;
+
+public class Cadena {
+
+	public static void main(String[] args) {
+        String cadena = "Hola, me llamo Azahara y estoy realizando un trabajo de Entornos del Desarrollo.";
+        
+        System.out.println("Este sistema está recorriendo la cadena:");
+        for (int i = 0; i < cadena.length(); i++) {
+            char caracter = cadena.charAt(i);
+            System.out.println("Carácter en posición " + i + ": " + );
+        }
+    }
+
+}
